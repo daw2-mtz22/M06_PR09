86267aa515b9c777a1dfc681860c335b49468360
Aaron <martinezasensioaaron@fpllefia.com>
2023-06-06 18:57:08 +0200

    v.01

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import './scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-import {perfiles} from "./componentes/perfiles.js";
 
 //Importamos el componente por defecto
 const componenteHome = await import('./vistas/home')
@@ -14,7 +13,6 @@ const homeVista = componenteHome.default
 //Inyectamos el componente header
 document.querySelector('header').innerHTML = header.template
 //Inyectamos la vista home
-document.querySelector('main').innerHTML = perfiles.template
-perfiles.script()
+document.querySelector('main').innerHTML = homeVista.template
 //Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
